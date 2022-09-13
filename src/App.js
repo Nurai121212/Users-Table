4771e9c2886c47dfe0a2b4135da181c1bb209a01
Nurai <nooraselfeght@gmail.com>
2022-09-13 14:06:04 +0600

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import './App.sass';
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [select, setSelect] = useState("phone");
+
+  const columns = ["name", "email", ["phone", "website", "username"]];
+
+  const handleChange = (e) => {
+    setSelect(e.target.value)
+  };
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then((res) => {
+        setItems(res.data)
+      }).catch((e) => {
+        console.log(e);
+      }).finally(() => {
+        setLoading(false)
+      })
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      {loading ? <h1>Загрузка...</h1> : items.length ? 
+        <table className="users-table">
+          <thead>
+            <tr>{columns.map((item, i) => {
+                return(
+                  <th key={i}>
+                    {!Array.isArray(item) ? item : 
+                      <select value={select} onChange={handleChange}>
+                        {item.map((item, i) => {
+                          return(
+                            <option value={item} key={i}>{item}</option>
+                          )
+                        })}
+                      </select>}
+                  </th>
+                )
+              })}
+            </tr>
+          </thead>
+          <tbody>
+            {items.map(item => {
+              return(
+                <tr key={item.id}>
+                  <td>{item.name}</td>
+                  <td>{item.email}</td>
+                  <td>{item[select]}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>  :  <h1>Данные не загрузились.</h1>}
     </div>
   );
 }
